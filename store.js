570f6d598a40636466299090b8a0d39309644f48
Damian Wolkowicz <damianwolkowicz9@gmail.com>
2020-03-28 19:45:29 +0100

    Dodano możliwość zmiany statusu zadania

--- store.js
+++ store.js
@@ -19,7 +19,8 @@ const createTask = async (title, description) => (
       PartitionKey: generator.String('task'),
       RowKey: generator.String(uuid.v4()),
       title,
-      description
+      description,
+      status: 'open'
     }
 
     !title ? function() { alert("Nie podano tytulu"); return reject(); } : service.insertEntity(table, task, (error, result, response) => {
@@ -31,15 +32,32 @@ const createTask = async (title, description) => (
 const listTasks = async () => (
   new Promise((resolve, reject) => {
     const query = new storage.TableQuery()
-      .select(['title'], ['description'], ['Timestamp'])
+      .select(['title'], ['description'], ['Timestamp'], ['status'], ['RowKey'])
       .where('PartitionKey eq ?', 'task')
 
     service.queryEntities(table, query, null, (error, result, response) => {
       !error ? resolve(result.entries.map((entry) => ({
         title: entry.title._,
         description: !entry.description._ ? "Brak opisu" : entry.description._,
-        Timestamp: entry.Timestamp._
+        Timestamp: entry.Timestamp._,
+        id: entry.RowKey._,
+        status: entry.status._
       }))) : reject()
+    })
+  })
+)
+
+const updateTaskStatus = async (id, status) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(id),
+      status
+    }
+  
+    service.mergeEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
     })
   })
 )
@@ -47,5 +65,6 @@ const listTasks = async () => (
 module.exports = {
   init,
   createTask,
-  listTasks
+  listTasks,
+  updateTaskStatus
 }
